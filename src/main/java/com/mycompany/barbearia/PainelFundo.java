5fb202b0607afc6f8b1289a8c4a4bcc2dae680a3
Marlo <Marlo@DESKTOP-VB7FH9A>
2024-09-15 20:56:09 -0700

    2 adicionei barbearia

--- src/main/java/com/mycompany/barbearia/PainelFundo.java
+++ src/main/java/com/mycompany/barbearia/PainelFundo.java
@@ -1,5 +1,30 @@
 package com.mycompany.barbearia;
 
-public class PainelFundo {
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class PainelFundo extends JPanel {
+    private BufferedImage imagemFundo;
+    
+    public PainelFundo(String caminho){
+        try{
+        imagemFundo = ImageIO.read(new File(caminho));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        if(imagemFundo != null){
+            g.drawImage(imagemFundo, 0, 0,getWidth(), getHeight(), this);
+        }
+    }
     
 }
