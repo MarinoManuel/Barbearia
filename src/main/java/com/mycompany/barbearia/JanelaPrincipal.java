d27a1ce0aaee48a3eef5b28c13285c4168fd8801
Marlo <Marlo@DESKTOP-VB7FH9A>
2024-09-15 20:22:19 -0700

    2 adicionei barbearia

---
+++ src/main/java/com/mycompany/barbearia/JanelaPrincipal.java
@@ -0,0 +1,14 @@
+package com.mycompany.barbearia;
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+
+public class JanelaPrincipal extends JFrame {
+
+    public JanelaPrincipal(){
+        super("BarberShop");
+        
+    }
+    
+}
