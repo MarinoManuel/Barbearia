77e93736f4abe559fed7c0a493b1ed3cb7ccac20
Marlo <Marlo@DESKTOP-VB7FH9A>
2024-09-25 14:36:16 -0700

    funcoes BD Marlo

--- src/main/java/com/mycompany/barbearia/Controlador.java
+++ src/main/java/com/mycompany/barbearia/Controlador.java
@@ -1,78 +1,57 @@
 package com.mycompany.barbearia;
 
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.SwingUtilities;
 
 public class Controlador {
 
-    private static Object chave = new Object();
-    private static boolean verificadorChave;
-    private static boolean estadoConexao;
-
     static JanelaPrincipal janelaPrincipal;
     static PainelLogin painelLogin;
 
-    Controlador() {
-        janelaPrincipal = new JanelaPrincipal();
-        painelLogin = new PainelLogin();
-        janelaPrincipal.add(painelLogin);
-        primeiraIteracao();
-    }
-
-    public static void primeiraIteracao() {
+    Controlador() throws InterruptedException {
+        Conexao.Conexao();
         SwingUtilities.invokeLater(() -> {
-            verificadorChave = false;
-            painelLogin.removerTudo();
+            janelaPrincipal = new JanelaPrincipal();
+            painelLogin = new PainelLogin();
+            janelaPrincipal.add(painelLogin);
             painelLogin.addPainelCentralizado();
-            painelLogin.revalidate();
-            painelLogin.repaint();
-            janelaPrincipal.repaint();
-            janelaPrincipal.revalidate();
+            janelaPrincipal.r();
+        });
+        Thread.sleep(4000);
 
-            Thread verificarInternet = new Thread(() -> {
-                synchronized (chave) {
-                    estadoConexao = VerificacaoConexao.VerificarConexao();
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException ex) {
-                        ex.printStackTrace();
-                    }
-                    verificadorChave = true;
-                    chave.notify();
-                }
-            });
-            verificarInternet.start();
-
-            Thread oquefazer = new Thread(() -> {
-
-                synchronized (chave) {
-                    while (!verificadorChave) {
-                        try {
-                            chave.wait();
-                        } catch (InterruptedException ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-
-                    if (estadoConexao == true) {
-                        System.out.println("all Right programer");
-                        System.out.println("estado if");
-                    } else {
-                        System.out.println("estado else");
-                        painelLogin.removerTudo();
-                        painelLogin.addpainelBotaoReconectar();
-                        janelaPrincipal.repaint();
-                        janelaPrincipal.revalidate();
-                    }
-                }
-            });
-            oquefazer.start();
+        SwingUtilities.invokeLater(() -> {
+            painelLogin.removerTudo();
+            painelLogin.addPainelEntrada();
+            janelaPrincipal.r();
         });
     }
 
-    public static void listenerBotaoReconectar() {
-        primeiraIteracao();
+    public static void removerPainelLogin(){
+        janelaPrincipal.remove(painelLogin);
+    }
+    
+    public static void listenerBotaoEntrar() {
+        SwingUtilities.invokeLater(() -> {
+            byte a = ValidacaoUsuario.ValidarConsultausuario(painelLogin.getUsuario(), painelLogin.getPassword());
+            switch (a) {
+                case 0:
+                    removerPainelLogin();
+                    janelaPrincipal.r();
+                    break;
+                case 1:
+                    painelLogin.mudarEstadoVarivelLogin1();
+                    janelaPrincipal.r();
+                    break;
+                case 2:
+                    painelLogin.mudarEstadoVarivelLogin2();
+                    janelaPrincipal.r();
+                    break;
+                case 3:
+                    painelLogin.mudarEstadoVarivelLogin3();
+                    janelaPrincipal.r();
+                    break;
+            }
+        });
     }
 }
