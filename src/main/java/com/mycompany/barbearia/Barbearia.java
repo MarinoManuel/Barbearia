1f36bee37561aad65e8dab98efee6c1dbbdf34b9
Marlo <Marlo@DESKTOP-VB7FH9A>
2024-09-25 14:35:39 -0700

    funcoes BD Marlo

--- src/main/java/com/mycompany/barbearia/Barbearia.java
+++ src/main/java/com/mycompany/barbearia/Barbearia.java
@@ -1,43 +1,13 @@
 package com.mycompany.barbearia;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.List;
+import java.awt.BorderLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class Barbearia {
 
-    public static void main(String[] args) throws SQLException {
-        Conexao.Conexao();
-        // Substitua "SEU_ID_RECIBO" pelo ID do recibo que você deseja testar
-        String idRecibo = "recibo123";
-
-        try {
-            // Chama o método para obter recibos com relações
-            List<ReciboCompleto> recibosComRelacoes = Funcoes_BD.obterRecibosComRelacoes(idRecibo);
-
-            // Verifica se a lista retornada não está vazia
-            if (recibosComRelacoes.isEmpty()) {
-                System.out.println("Nenhum recibo encontrado para o ID: " + idRecibo);
-            } else {
-                // Imprime os recibos completos com suas relações
-                for (ReciboCompleto reciboCompleto : recibosComRelacoes) {
-                    System.out.println("ID do Recibo: " + reciboCompleto.getIdRecibo());
-                    System.out.println("Data do Recibo: " + reciboCompleto.getDataRecibo());
-                    System.out.println("Data do Atendimento: " + reciboCompleto.getDataAtendimento());
-                    System.out.println("Estado: " + reciboCompleto.getEstado());
-                    System.out.println("Nome do Usuário: " + reciboCompleto.getUserName());
-                    System.out.println("Nome do Serviço: " + reciboCompleto.getNomeServico());
-                    System.out.println("Tipo do Serviço: " + reciboCompleto.getTipoServico());
-                    System.out.println("Número de Telefone: " + reciboCompleto.getNumTelefone());
-                    System.out.println("Nome do Barbeiro: " + reciboCompleto.getNomeBarbeiro());
-                    System.out.println("-------------------------");
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+    public static void main(String[] args) throws InterruptedException {
+        new Controlador();
     }
 }
