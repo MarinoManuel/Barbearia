a66f7f0e807eeaea9730f80dc1cca59b37b57b77
Marlo <Marlo@DESKTOP-VB7FH9A>
2024-10-01 00:06:29 -0700

    adicao de painelAlgo

--- src/main/java/com/mycompany/barbearia/Conexao.java
+++ src/main/java/com/mycompany/barbearia/Conexao.java
@@ -12,6 +12,15 @@ public class Conexao {
     private static Connection connection;
 
     public static Connection Conexao() {
+        /*
+        create table if not exists usuarios(username varchar(45), userpassword varchar(45) not null, primary key(username));
+        create table if not exists clientes(numtelefone varchar(14), nomecliente varchar(15) not null, apelidocliente varchar(15) not null, email varchar(45), primary key(numtelefone) );
+        create table if not exists servicos(nomeservico varchar(45), tiposervico varchar(45), preco decimal not null, primary key(nomeservico, tiposervico) );
+        create table if not exists barbeiros(nomebarbeiro varchar(45) primary key);
+        create table if not exists reciboRelacoes(idRecibo varchar(45) , username varchar(45) , nomeservico varchar(45) , tiposervico varchar(45) , numtelefone varchar(15)  , nomebarbeiro varchar(45) ,
+        primary key(idrecibo, username, nomeservico, tiposervico, numtelefone, nomebarbeiro),foreign key (idrecibo) references Recibos(idrecibo), foreign key (username) references usuarios(username), foreign key (nomeservico, tiposervico) references servicos(nomeservico, tiposervico), foreign key (numtelefone) references clientes(numtelefone), foreign key (nomebarbeiro) references barbeiros(nomebarbeiro));
+        create table recibos(idRecibo varchar(45) primary key, datarecibo datetime, dataAtendimento datetime, estado tinyint(1));
+         */
 
         String url = "jdbc:mysql://localhost:3306/barbearia";
         String user = "root";
